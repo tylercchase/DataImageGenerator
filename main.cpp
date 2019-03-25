c03328920c731a2bd5698f8e0c6be10ef33d615f
tylercchase <tylercchase@gmail.com>
2019-03-25 12:56:51 -0800

    Add basic functionality, take input and turn into image

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
 
-#include "BinaryConvert.hpp"
 int main() {
 
 
@@ -10,22 +10,40 @@ int main() {
     std::string userInput;
     getline(std::cin, userInput);
 
+    std::vector<std::string> dataPoints;
 
     std::ofstream outfile ("test.ppm");
 
-    std::vector<std::vector<int>> dataPoints;
+
+    for(char & c : userInput){
+        dataPoints.push_back(std::bitset<8>((unsigned long long int)(c)).to_string());
+    }
+
+    for(std::string & r : dataPoints){
+        std::cout << r << std::endl;
+    }
 
     outfile << "P2\n"
-               "24 8\n"
-               "15\n"
-               "0  15  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
-               "0  0  3  3  3  0  0  7  7  7  7  0  0 11 11 11 11  0  0 15 15 15 15  0\n"
-               "0  15  0  0  0  0  0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0 15  0\n"
-               "0  15  3  3  0  0  0  7  7  7  0  0  0 11 11 11  0  0  0 15 15 15 15  0\n"
-               "0  0  0  0  0  0  0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0  0  0\n"
-               "0  15  0  0  0  0  0  7  7  7  7  0  0 11 11 11 11  0  0 15  0  0  0  0\n"
-               "0  15  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
-               "0  15  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0"<< std::endl;
+               "8 " << dataPoints.size() << "\n"
+               "1\n";
+
+    for(std::string & character : dataPoints){
+        for(char & c : character){
+            outfile << c << " ";
+        }
+        outfile << "\n";
+    }
+    outfile << std::endl;
+
+//
+//               "0  1  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  0  0  0  0  0  0  1  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  1  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  1  0\n"
+//               "0  1  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  0  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  1  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  1  0  0  0  0  0  1  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+//               "0  1  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0"<< std::endl;
 
     outfile.close();
     return 0;
