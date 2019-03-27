8263d1229afa699229dc579b699f1f02d187a644
tylercchase <tylercchase@gmail.com>
2019-03-27 11:09:54 -0800

    Add basic user menu functionality

---
+++ menuOptions.cpp
@@ -0,0 +1,4 @@
+//
+// Created by tyler on 3/26/19.
+//
+
---
+++ menuOptions.hpp
@@ -0,0 +1,8 @@
+//
+// Created by tyler on 3/26/19.
+//
+
+#ifndef QRCODE_MENUOPTIONS_HPP
+#define QRCODE_MENUOPTIONS_HPP
+
+#endif //QRCODE_MENUOPTIONS_HPP
